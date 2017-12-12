9ad9d7acf166bf8c6ab4c49b2ea0a661e1c3cf2b
Alvin Cheng <pop199890@gmail.com>
2017-12-12 16:30:33 +0800

    GAME2

--- src/alvin/DEC12.java
+++ src/alvin/DEC12.java
@@ -5,31 +5,42 @@ import java.util.Scanner;
 public class DEC12 {
 
 	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
-		int d = -1;
 		int hp = 100;
 		int pos = 0;
 		int row = 3;
 		int col = 5;
+		Scanner scanner = new Scanner(System.in);
+		int d = -1;
 		while (d != 0) {
-			System.out.println("Please enter an number:");
+			System.out.println("目前位置:"+pos+"Hp"+hp);
+			System.out.print("請輸入方向:");
 			String line = scanner.nextLine();
 			int i = Integer.parseInt(line);
 			switch (i) {
-			case 8:
-				System.out.println("向上");
+			case 2:
+				System.out.println("向下");
 				if (pos / col < row - 1) {
 					pos = pos + col;
 					hp = hp-5;
 					System.out.println(hp);
 				}else{
 					hp = hp-30;
-					System.out.println(hp);
+				}
 					break;
-
+			case 4 :
+				System.out.println("向左");
+				if (pos % col != 0){
+					pos = pos - col;
+					hp = hp-5;
+				}else{
+					hp = hp-30;
+				}
+				break;
+				
+						
+				
 				}
 			}
 
 		}
 	}
-}
