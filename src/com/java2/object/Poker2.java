325264c9fad41cba82afaa046ddacd0791ededb4
Alvin Cheng <pop199890@gmail.com>
2018-03-27 16:26:18 +0800

    PokerList

---
+++ src/com/java2/object/Poker2.java
@@ -0,0 +1,25 @@
+package com.java2.object;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class Poker2 {
+	public static void main(String[] args) {
+		List<Integer> cards =new ArrayList<>();
+		cards.add(12);
+		cards.add(8);
+		cards.add(3);
+		cards.set(1, 99);
+		System.out.println("removing number:"+cards.remove(0));
+		System.out.println(cards.size());
+		System.out.println(cards.get(1));
+		
+	}
+
+}
+			
+		
+		
+	
+
+
