49924c02ec8fcb87cf828fcb5c00e96b6dde3862
Alvin Cheng <pop199890@gmail.com>
2018-04-10 16:26:12 +0800

    IO

---
+++ src/com/java2/io/FileTester.java
@@ -0,0 +1,26 @@
+package com.java2.io;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class FileTester {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		try{
+			FileOutputStream out = new FileOutputStream("data.txt");
+			out.write(65);
+			out.flush();
+			out.close();
+		}catch (FileNotFoundException e) {
+			System.out.println("File NOT FOUND");
+			
+		}catch (IOException e){
+			System.out.println("File output problem");
+			
+		}
+
+	}
+
+}
