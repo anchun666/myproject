5ccc6ea47623a32f874082fa795e01aec24293cb
user <user@es301n36>
2019-05-28 12:00:08 +0800

    0528

---
+++ src/com/alvin2/Horse.java
@@ -0,0 +1,5 @@
+package com.alvin2;
+
+public class Horse {
+
+}
---
+++ src/com/alvin2/Test.java
@@ -0,0 +1,10 @@
+package com.alvin2;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
